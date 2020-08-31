f1c68226ddf777a057e1cf48628c5d0ede1746cf
Thiago Zimmermann <thiago-dev902@outlook.com>
2020-08-31 16:37:07 +0200

    add missing ArgsReader.cs file

---
+++ src/Hypnonema.Client/ArgsReader.cs
@@ -0,0 +1,43 @@
+﻿namespace Hypnonema.Client
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+
+    using CitizenFX.Core;
+
+    public static class ArgsReader
+    {
+        public static T GetArgKeyValue<T>(IDictionary<string, object> args, string key, T defaultValue)
+        {
+            var result = defaultValue;
+
+            try
+            {
+                var input = args.FirstOrDefault(arg => arg.Key == key).Value?.ToString();
+                result = (T)TypeDescriptor.GetConverter(typeof(T)).ConvertFromString(input);
+            }
+            catch (Exception)
+            {
+                Debug.WriteLine($"failed to read {key}");
+            }
+
+            return result;
+        }
+
+        public static T GetArgKeyValue<T>(IDictionary<string, object> args, string key)
+        {
+            var result = default(T);
+            try
+            {
+                result = GetArgKeyValue(args, key, default(T));
+            }
+            catch (Exception)
+            {
+            }
+
+            return result;
+        }
+    }
+}
